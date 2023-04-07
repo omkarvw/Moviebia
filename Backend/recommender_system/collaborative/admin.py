f28d2aa1a16613351767622ad8ad493070b85425
beakbreak <namanbhagat1414@gmail.com>
2023-04-07 22:18:22 +0530

    data changed

--- Backend/recommender_system/collaborative/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Movie, Rating, Suggestion, userid
-# Register your models here.
-admin.site.register(Movie)
-admin.site.register(Rating)
-admin.site.register(Suggestion)
-admin.site.register(userid)
--- Backend/recommender_system/collaborative/paginations.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class CustomNumberPagination(PageNumberPagination):
-    page_size = 15
-
-class SuggestionsPagination(PageNumberPagination):
-    page_size = 10
